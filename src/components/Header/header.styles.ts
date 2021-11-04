9a3dd2d52c92d0d3a5dd28c5d8805d9dc275f2ee
Sebastian <sebastian.zawolanski@gmail.com>
2021-11-04 19:39:25 +0100

    refactor: remove the eslint comment

--- src/components/Header/header.styles.ts
+++ src/components/Header/header.styles.ts
@@ -2,7 +2,6 @@ import { styled } from '@mui/system';
 import { AppBar, Button, Toolbar } from '@mui/material';
 
 export const CustomAppBar = styled(AppBar)(({ theme }) => ({
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   zIndex: 1,
 
   [theme.breakpoints.up('md')]: {
