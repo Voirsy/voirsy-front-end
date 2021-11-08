4960f9295675c55caf10b65b69825c01c764bb97
Sebastian <sebastian.zawolanski@gmail.com>
2021-11-08 17:12:47 +0100

    feat: create the Spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,10 @@
+import { CircularProgress } from '@mui/material';
+import { CustomBox } from './spinner.styled';
+
+const Spinner = () => (
+  <CustomBox>
+    <CircularProgress />
+  </CustomBox>
+);
+
+export default Spinner;
