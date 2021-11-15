8a117f3278e35113d86ad0e11e87ac6f4656b8a4
Sebastian <sebastian.zawolanski@gmail.com>
2021-11-15 12:37:45 +0100

    Remove a needless components

--- src/templates/Salons/salons.styled.ts
+++ src/templates/Salons/salons.styled.ts
@@ -2,23 +2,6 @@
 import { Box, styled } from '@mui/system';
 import theme from 'theme';
 
-export const CustomBox = styled(Box)(() => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  height: `calc(100vh - ${theme.mixins.toolbar.minHeight}px)`,
-
-  [theme.breakpoints.up('sm')]: {
-    height: `calc(100vh - ${(theme.mixins.toolbar['@media (min-width:600px)'] as any).minHeight}px)`,
-  },
-
-  '@media (orientation: landscape)': {
-    height: `calc(100vh - ${
-      (theme.mixins.toolbar['@media (min-width:0px) and (orientation: landscape)'] as any).minHeight
-    }px)`,
-  },
-}));
-
 export const CustomEditHeader = styled(Box)(() => ({
   display: 'flex',
   justifyContent: 'space-between',
