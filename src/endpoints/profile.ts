e462567fd0ea4f9cc2715f5efdf0f640444a31bb
Sebastian <sebastian.zawolanski@gmail.com>
2022-01-13 16:07:10 +0100

    feat: connect the change password view with the backend

--- src/endpoints/profile.ts
+++ src/endpoints/profile.ts
@@ -1,3 +1,4 @@
 export const PROFILE = {
   FAVORITES: '/profile/favorites',
+  CHANGE_PASSWORD: '/profile',
 };
--- src/store/api/profile.ts
+++ src/store/api/profile.ts
@@ -2,10 +2,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { ENV } from 'config/enviroments';
 import { PROFILE } from 'endpoints/profile';
 import { Salon } from 'models/admin.model';
+import { getToken } from '../../helpers/auth';
 
 export const profileApi = createApi({
   reducerPath: 'profileApi',
-  baseQuery: fetchBaseQuery({ baseUrl: ENV.apiUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: ENV.apiUrl,
+    prepareHeaders: (headers) => {
+      const token = `Bearer ${getToken()}`;
+
+      if (token) headers.set('Authorization', token);
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({
     fetchAllUsersFavorites: builder.query<
       Pick<Salon, '_id' | 'address' | 'name' | 'imageUrl' | 'city' | 'rating' | 'type'>[],
@@ -13,7 +22,14 @@ export const profileApi = createApi({
     >({
       query: () => `${PROFILE.FAVORITES}`,
     }),
+    changePassword: builder.mutation<{ message: string }, { oldPassword: string; newPassword: string }>({
+      query: (body) => ({
+        url: `${PROFILE.CHANGE_PASSWORD}`,
+        method: 'POST',
+        body,
+      }),
+    }),
   }),
 });
 
-export const { useFetchAllUsersFavoritesQuery } = profileApi;
+export const { useFetchAllUsersFavoritesQuery, useChangePasswordMutation } = profileApi;
