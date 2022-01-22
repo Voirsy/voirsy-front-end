b916dc96c14353046b1c80593584813075b3cedf
Sebastian <sebastian.zawolanski@gmail.com>
2022-01-22 20:07:33 +0100

    fix: admin menu

--- src/components/AdminPanelNavigation/index.tsx
+++ src/components/AdminPanelNavigation/index.tsx
@@ -1,15 +1,21 @@
 import { CloseOutlined } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+import { IconButton, useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useFetchAllSalonsQuery } from 'store/api/admin/admin';
 import { CustomDrawer, CustomToolbar } from './adminPanelNavigation.styled';
 import SalonsNavigation from './adminPanelNavigation.list';
+import theme from 'theme';
 
 const AdminPanelNavigation = ({ isMenuOpen, handleClose }: { isMenuOpen: boolean; handleClose: () => void }) => {
   const location = useLocation();
   const { data = { salons: [], message: '' } } = useFetchAllSalonsQuery();
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
+
+  useEffect(() => {
+    if (!matches) handleClose();
+  }, [matches]);
 
   useEffect(() => handleClose(), [location]);
 
